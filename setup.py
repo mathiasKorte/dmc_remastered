46d597eba78d3509ec3007d0aeb6010b81e8ffe3
Mathias Korte <ga27bup@mytum.de>
2024-09-11 19:45:33 +0200

    include assets folder to allow "pip install git+..."

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jakegrigsby/dmc_remastered",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     python_requires='>=3.6',
     install_requires=[
         "gym",
